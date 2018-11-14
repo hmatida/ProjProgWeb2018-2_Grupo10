24bab4e33fe63c3583a51cd0c530183d5339a672
Hernane Matida <linehcm@hotmail.com>
2018-11-14 20:37:19 -0300

    Arrumando o Controller

--- src/main/java/br/com/rentamovie/ram/controller/HomeController.java
+++ src/main/java/br/com/rentamovie/ram/controller/HomeController.java
@@ -1,30 +1,38 @@
 package br.com.rentamovie.ram.controller;
 
-import br.com.rentamovie.ram.model.entities.Movie;
+import br.com.rentamovie.ram.model.entities.Person;
 import br.com.rentamovie.ram.model.repositories.MovieRepo;
-import br.com.rentamovie.ram.model.services_and_utilities.MovieService;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-    
+
     @Autowired
     private MovieRepo movieRepo;
 
     @GetMapping("/")
-    public ModelAndView start(){
+    public ModelAndView start() {
         ModelAndView mod = new ModelAndView("index");
-           return mod.addObject("movies", movieRepo.findAll());
+        return mod.addObject("movies", movieRepo.findAll());
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login() {
+        return new ModelAndView("login");
     }
     
-    @GetMapping("/login")
-    public ModelAndView login(){
-        return new ModelAndView("login");
+    @GetMapping("/novo")
+    public ModelAndView novo() {
+        return new ModelAndView("novo");
+    }
+
+    @PostMapping("/cadastra")
+    public ModelAndView salva(@Valid Person person) {
+        return new ModelAndView("novo");
     }
 }
